29f05916b2a66173e3988a12baed4bba2642a616
Corentin PRUNE <corentin.prune@qonto.com>
2025-01-26 19:34:23 +0100

    test everything is working with a basic test-app

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -2,14 +2,41 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+const plugin = require('ember-x-react').buildBabelPlugin();
+
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
     autoImport: {
       watchDependencies: ['ember-x-react'],
     },
+    babel: {
+      plugins: [plugin],
+    },
   });
 
   const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app);
+  return maybeEmbroider(app, {
+    packagerOptions: {
+      webpackConfig: {
+        module: {
+          rules: [
+            {
+              test: /\.tsx$/,
+              use: [
+                {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-react', { runtime: 'automatic' }],
+                    ],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+  });
 };
